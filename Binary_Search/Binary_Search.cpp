2739ac64ad4d6726eb1a2f1e475d5498c9339109
Himanshu Kumar <himanshukr0825@gmail.com>
2026-09-04 18:52:12 +0530

    Add Binary Search Solution

---
+++ Binary_Search/Binary_Search.cpp
@@ -0,0 +1,30 @@
+// Problem: Binary Search
+// Source: LeetCode
+// Difficulty: Easy
+// Topic: Binary Search
+// Solution: Optimal
+// Algorithm: Recursive Binary Search
+// Time Complexity: O(log n)
+// Auxiliary Space: O(log n)
+
+class Solution {
+public:
+    int bs(vector<int>& nums, int low, int high, int target) {
+        if (low > high)
+            return -1;
+
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        else if (target > nums[mid])
+            return bs(nums, mid + 1, high, target);
+
+        return bs(nums, low, mid - 1, target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        return bs(nums, 0, nums.size() - 1, target);
+    }
+};
