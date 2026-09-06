ab74a71e54ac91637b31b687cf01b8ce3c9222c5
Himanshu Kumar <himanshukr0825@gmail.com>
2026-09-07 00:48:14 +0530

    Add Search in Rotated Sorted Array Solution

---
+++ Binary_Search/Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,44 @@
+// LeetCode: Search in Rotated Sorted Array
+// Difficulty: Medium
+// Topic: Binary Search
+// Solution: Optimal
+// Algorithm: Modified Binary Search
+// Time Complexity: O(log n)
+// Auxiliary Space: O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            // Left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
